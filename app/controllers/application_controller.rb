8ababd10a8e088bf42c7147ce638709322c807d2
Brian Lambert <brianlambert2408@gmail.com>
2022-11-23 10:02:30 +1100

    commit tree cb18383d1e139932518682287bb387ac939904b0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, "application/json"
+
   get '/' do
     { message: "Hello world" }.to_json
   end
+  get '/games' do
+    Game.all.to_json
+  end
 
+  get '/games/:id' do
+    id = params["id"].to_i
+    game = Game.find(id)
+    game.to_json(
+      only: [:id, :title, :genre, :price], 
+      include: {
+        reviews: { only: [:comment, :score], 
+          include: { 
+            user: { only: [:name] }
+          } 
+        }
+      })
+  end
 end
